843e954c2290c7f63d636495d0ce0068808d4233
Loky <puiualexandru87@yahoo.com>
2020-07-08 20:26:14 +0200

    added a new system out println because I want it to test git hub and the commit and push function

--- src/classInheritanceTwo/Main3.java
+++ src/classInheritanceTwo/Main3.java
@@ -11,6 +11,10 @@ public class Main3 {
         Entity entity3 = new Lion();
         Entity entity4 = new Lion( 10 );
 
+        int a = 2;
+        int b = 3;
+        byte c = (byte) (a + b); // casting to a byte
+        System.out.println(c);
         System.out.println(entity3);
         System.out.println(entity4);
 
